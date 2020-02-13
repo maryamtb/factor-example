615c7fe22fd9b9e6045708ae41dcdc8d8d0fee5f
Maryam <maryamtb.17@gmail.com>
2020-02-13 18:50:01 +0100

    zeno theme

---
+++ src/factor-settings.ts
@@ -0,0 +1,10 @@
+/**
+ * Factor Settings - How settings work in Factor...
+ *
+ * Your settings files are how you customize themes and plugins.
+ * They allow you to set text and images, but also override components, routes and more.
+ *
+ */
+
+
+export default {}
